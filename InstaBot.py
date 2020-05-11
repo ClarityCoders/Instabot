363423eda47ccec39491ce6a99331da484fbfce2
Jake Lower <jake@claritycoders.com>
2020-05-11 07:20:52 -0500

    Update bot

--- InstaBot.py
+++ InstaBot.py
@@ -5,70 +5,63 @@ import time
 def login(browser):
     browser.get("https://www.instagram.com/?hl=en")
     time.sleep(5)
+    username = browser.find_element_by_css_selector("[name='username']")
+    password = browser.find_element_by_css_selector("[name='password']")
+    login = browser.find_element_by_css_selector("button")
 
-    username_element = browser.find_elements_by_css_selector( "[aria-label='Phone number, username, or email']" ) 
-    password_element = browser.find_elements_by_css_selector( "[aria-label='Password']" )
-    login_button = browser.find_elements_by_css_selector("button")
-    # "button[class='sqdOP  L3NKy   y3zKF     ']"
+    #YOUR USERNAME GOES HERE
+    username.send_keys("my_username")
+    #YOUR Password GOES HERE
+    password.send_keys("mypass291")
+    login.click()
 
-    input_username = username_element[0]
-    input_password = password_element[0]
-    input_login = login_button[0]
 
-    input_username.send_keys("j_low_17")
-    input_password.send_keys("test17!91")
-    input_login.click()
+
     time.sleep(5)
 
-def Visit_Tag(browser, url):
-    sleepy_time = 4
+
+def Vist_Tag(browser, url):
+    sleepy_time = 5
+    browser.get(url)
+    time.sleep(sleepy_time)
+
+    pictures = browser.find_elements_by_css_selector("div[class='_9AhH0']")
+
     image_count = 0
-    try:
-        browser.get(url)
+
+    for picture in pictures:
+        if image_count >= 3:
+            break
+
+        picture.click()
         time.sleep(sleepy_time)
-        
-        input1_elem = browser.find_elements_by_css_selector("div[class='_9AhH0']")
-        for image in input1_elem:
-            if image_count > 3:
-                break
-            image.click()
-            time.sleep(sleepy_time)
-            no_results = browser.find_elements_by_css_selector("[aria-label='Unlike']")
-            image_count += 1
-            if len(no_results) == 0:
-                heart_locator = "button[class='wpO6b ']"
-                heart_elm = browser.find_elements_by_css_selector(heart_locator)
-                heart_button = heart_elm[0]
-                heart_button.click()
-                time.sleep(sleepy_time)
-            close = browser.find_elements_by_css_selector("[aria-label='Close']")
-            print(close[0])
-            close[0].click()
-        
 
-    except Exception as e:
-        print("error in process_jobs")
-        print("\n")
-        print(str(e) + "\n")
+        heart = browser.find_element_by_css_selector("[aria-label='Like']")
+        heart.click()
 
+        close = browser.find_element_by_css_selector("[aria-label='Close']")
+        close.click()
+
+        image_count += 1
+        time.sleep(sleepy_time)
 
 def main():
-    tags = [ 
-            "programming",
-            "softwaredeveloper",
-            "programminglife",
-            "programmerslife",
-            "programmerlife",
-            "developerlife",
-            "programmers",
-        ]
     browser = webdriver.Chrome()
     login(browser)
 
-    for tag in tags:
-        Visit_Tag(browser, f"https://www.instagram.com/explore/tags/{tag}/")
+    tags = [ 
+        "programming",
+        "softwaredeveloper",
+        "programminglife",
+        "programmerslife",
+        "programmerlife",
+        "developerlife",
+        "programmers",
+    ]
 
-    
-
+    while True:
+        for tag in tags:
+            Vist_Tag(browser, f"https://www.instagram.com/explore/tags/{tag}")
+        time.sleep(3600)
 
 main()
